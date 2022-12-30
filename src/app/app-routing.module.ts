2373b64b038afedbe1ccbb8b23a70bf3913ce6fd
ShivaKumarsp <31365122+ShivaKumarsp@users.noreply.github.com>
2022-12-30 19:01:07 +0530

    csdc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Test2Component } from './component/test2/test2.component';
 import { Test3Component } from './component/test3/test3.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'test1' },
+  { path: '', pathMatch: 'full', redirectTo: 'test2' },
   {path:'test1',component:Test1Component},
   {path:'test2',component:Test2Component},
   {path:'test3',component:Test3Component},
