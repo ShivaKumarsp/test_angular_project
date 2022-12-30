f62ef5fc06a924f194f75cf3ff7e9d04d22243fc
shivakumar <shivakumar@avaniinfosoft.com>
2022-12-30 17:55:40 +0530

    latest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Test1Component } from './component/test1/test1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'test1' },
+  {path:'test1',component:Test1Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
