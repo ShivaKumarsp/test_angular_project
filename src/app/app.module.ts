bf08587df70da530881d3b38a1eb1d3479588586
shivakumar <shivakumar@avaniinfosoft.com>
2022-12-30 18:08:23 +0530

    scsc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Test1Component } from './component/test1/test1.component';
+import { Test2Component } from './component/test2/test2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Test1Component
+    Test1Component,
+    Test2Component
   ],
   imports: [
     BrowserModule,
